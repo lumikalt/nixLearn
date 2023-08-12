2e1c1462735cfd6ebb70854f31f023104f63bd4c
Lumi Kalt <lumi.mia.kalt@gmail.com>
2023-08-12 16:20:02 +0100

    Works

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Learning to use nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-bin.beta.latest.default
+          ];
+        };
+      }
+  );
+}
